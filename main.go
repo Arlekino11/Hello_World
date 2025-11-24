07de9692a0bf46f259bc0aba232160db28343991
Arlekino11 <arlekinocrusher@gmail.com>
2025-11-24 14:47:39 +0300

    feat: add GitHub Actions CI

--- main.go
+++ main.go
@@ -7,14 +7,21 @@ import (
 	"time"
 )
 
+type InfoResponse struct {
+	Author    string   `json:"author"`
+	Version   string   `json:"version"`
+	Endpoints []string `json:"endpoints"`
+}
+
 type HealthResponse struct {
 	Status    string `json:"status"`
 	Timestamp string `json:"timestamp"`
 }
 
 func main() {
+	http.HandleFunc("/info", infoHandler)
 	http.HandleFunc("/health", healthHandler)
-	http.HandleFunc("/greet", greetHendler)
+	http.HandleFunc("/greet", greetHandler)
 	http.HandleFunc("/", helloHandler)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
@@ -27,7 +34,7 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World!")
 }
 
-func greetHendler(w http.ResponseWriter, r *http.Request) {
+func greetHandler(w http.ResponseWriter, r *http.Request) {
 	name := r.URL.Query().Get("name")
 	if name == "" {
 		name = "Anonymous"
@@ -37,11 +44,22 @@ func greetHendler(w http.ResponseWriter, r *http.Request) {
 }
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Contetn-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 
 	response := HealthResponse{
-		Status:    "healty",
+		Status:    "healthy",
 		Timestamp: time.Now().Format(time.RFC3339),
+	}
+	json.NewEncoder(w).Encode(response)
+}
+
+func infoHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	response := InfoResponse{
+		Author:    "Arlekino",
+		Version:   "1.0.0",
+		Endpoints: []string{"/", "/health", "/greet", "/info"},
 	}
 	json.NewEncoder(w).Encode(response)
 }
